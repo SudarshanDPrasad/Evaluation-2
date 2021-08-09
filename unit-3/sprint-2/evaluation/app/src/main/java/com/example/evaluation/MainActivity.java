1def9e68c5eab65f7e580827617ac5173b969e43
sudararshan <sudarshanprasad18@gmail.com>
2021-08-09 14:27:17 +0530

    splash screen and fragment

--- unit-3/sprint-2/evaluation/app/src/main/java/com/example/evaluation/MainActivity.java
+++ unit-3/sprint-2/evaluation/app/src/main/java/com/example/evaluation/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.evaluation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView SplashScreen;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SplashScreen = findViewById(R.id.SplashScreen);
+
+        SplashScreen.animate().translationX(3000).translationY(3000).setStartDelay(3000);
+       new Handler().postDelayed(new Runnable() {
+           @Override
+           public void run() {
+               Intent intent = new Intent(MainActivity.this,ItemListActivity.class);
+               startActivity(intent);
+               finish();
+           }
+       },3000);
+
     }
 }
