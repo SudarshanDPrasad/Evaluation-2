7804aab5b9e9987f2879a2d651304ac927cfe6a2
sudararshan <sudarshanprasad18@gmail.com>
2021-08-23 18:01:53 +0530

    sumit

--- unit-3/sprint-4/evaluation/MusicPlayer/app/src/main/java/com/example/musicplayer/MusicService.kt
+++ unit-3/sprint-4/evaluation/MusicPlayer/app/src/main/java/com/example/musicplayer/MusicService.kt
@@ -1,4 +1,55 @@
 package com.example.musicplayer
 
-class MusicService {
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.media.MediaPlayer
+import android.os.Build
+import android.os.IBinder
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+
+
+class MusicService() : Service() {
+
+
+    private var mediaPlayer: MediaPlayer? = null
+
+    override fun onCreate() {
+        super.onCreate()
+        mediaPlayer = MediaPlayer.create(this,R.raw.songs)
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        startMusic()
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+
+
+    private fun startMusic() {
+        val thread = Thread(task)
+        thread.start()
+    }
+
+    private val task = Runnable {
+        mediaPlayer!!.start()
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+
+    fun play() {
+        if (!mediaPlayer!!.isPlaying) mediaPlayer!!.start()
+    }
+
+    fun pause() {
+        if (mediaPlayer!!.isPlaying) mediaPlayer!!.pause()
+    }
+
 }
--- unit-3/sprint-4/evaluation/MusicPlayer/app/src/main/java/com/example/musicplayer/PlayActivity.kt
+++ unit-3/sprint-4/evaluation/MusicPlayer/app/src/main/java/com/example/musicplayer/PlayActivity.kt
@@ -1,20 +1,30 @@
 package com.example.musicplayer
 
+import android.content.Intent
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_play.*
 
 class PlayActivity : AppCompatActivity() {
+
+    private var mediaPlayer: MediaPlayer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_play)
 
-        val bundle : Bundle?=intent.extras
+        mediaPlayer = MediaPlayer.create(this, R.raw.songs)
+
+        val bundle: Bundle? = intent.extras
         tvTrackName.text = bundle!!.getString("TrackName")
         tvArtistName.text = bundle!!.getString("ArtistName")
         tvCollectionName.text = bundle!!.getString("CollectionName")
         val imageid = bundle!!.getInt("Image")
         ivImageView.setImageResource(bundle!!.getInt("Image"))
-        
+
+
     }
+
+
 }
