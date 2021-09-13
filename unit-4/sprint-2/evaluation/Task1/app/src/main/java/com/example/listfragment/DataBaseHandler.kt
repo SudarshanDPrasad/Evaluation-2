adc69e44e177b985627f826713544092707b3a49
sudararshan <sudarshanprasad18@gmail.com>
2021-09-13 15:24:50 +0530

    database working

--- unit-4/sprint-2/evaluation/Task1/app/src/main/java/com/example/listfragment/DataBaseHandler.kt
+++ unit-4/sprint-2/evaluation/Task1/app/src/main/java/com/example/listfragment/DataBaseHandler.kt
@@ -1,8 +1,10 @@
 package com.example.listfragment
 
+import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import android.widget.Toast
 
 class DataBaseHandler(val context: Context) :
 SQLiteOpenHelper(context,"addTask",null,1){
@@ -14,7 +16,7 @@ SQLiteOpenHelper(context,"addTask",null,1){
         val ID = "id"
         val ITEM_NAME = "itemname"
         val DESCRIPTION = "description"
-        val PRICE = 0
+        val PRICE = "0"
     }
 
     override fun onCreate(db: SQLiteDatabase?) {
@@ -29,8 +31,47 @@ SQLiteOpenHelper(context,"addTask",null,1){
 
     }
 
-    fun inserTask(itemname: String,description:String,price:Int)
+    fun inserTask(itemname: String,description:String,price:Int){
+        val db = writableDatabase
+        val values = ContentValues()
+        values.put(ITEM_NAME,itemname)
+        values.put(DESCRIPTION,description)
+        values.put(PRICE,price)
 
+        val idvalue = db.insert(
+            TABLE_NAME,
+            null,
+            values
+        )
+        if(idvalue.toInt()==-1){
+            Toast.makeText(context, "Failed to Add the Item ", Toast.LENGTH_SHORT).show()
+        }else {
+            Toast.makeText(context, "Success to Add the Item ", Toast.LENGTH_SHORT).show()
+
+        }
+    }
+
+    fun getAllTask() : MutableList<AddModel>{
+        val tasklist = mutableListOf<AddModel>()
+        val db = readableDatabase
+        val query = "select * from $TABLE_NAME"
+        val cursor = db.rawQuery(query, null)
+        if(cursor!=null && cursor.count>0){
+            cursor.moveToFirst()
+
+
+            do {
+                val id = cursor.getInt(cursor.getColumnIndex(ID))
+                val item = cursor.getString(cursor.getColumnIndex(ITEM_NAME))
+                val desc = cursor.getString(cursor.getColumnIndex(DESCRIPTION))
+                val price = cursor.getInt(cursor.getColumnIndex(PRICE))
+
+                val addModel = AddModel()
+                addModel
+            }
+        }
+
+    }
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
         TODO("Not yet implemented")
     }
